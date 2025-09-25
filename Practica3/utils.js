a8ca8bf823b8ce15860f5b34743b058680135f03
RuthPina8 <124050978@upq.edu.mx>
2025-09-25 16:50:33 -0600

    Ejercicio1

---
+++ Practica3/utils.js
@@ -0,0 +1,11 @@
+// utils.js
+class Operaciones {
+  static restar(a, b) {
+    return a - b;
+  }
+}
+
+module.exports = Operaciones;
+
+// Funcion que ns ayuda a exportar al otro archivo esta seccion de codigo
+module.exports = restar;
