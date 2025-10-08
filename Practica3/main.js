8b2b29bc252ac4adcadce8f6258bab50b779bf10
RuthPina8 <124050978@upq.edu.mx>
2025-10-08 17:46:30 -0600

    Practica3

--- Practica3/main.js
+++ Practica3/main.js
@@ -1,8 +1,7 @@
-
-const restar = require("./utils"); // Aqui se utiliza el "./utils" para llamar del otro archivo con la resta
+const restar = require("./utils");// Aqui se utiliza el "./utils" para llamar del otro archivo con la resta
 
 // Pruebas, las puse asi como en ejercicios vistos anteriormente
-console.log("5 - 3 =", restar(5, 3));  // 2
-console.log("10 - 7 =", restar(10, 7)); // 3
-console.log("20 - 15 =", restar(20, 15)); // 5
-console.log("0 - 8 =", restar(0, 8)); // -8
+
+console.log(restar(10, 5));
+console.log(restar(7, 3));
+console.log(restar(20, 4));
--- Practica3/utils.js
+++ Practica3/utils.js
@@ -1,11 +1,7 @@
-// utils.js
-class Operaciones {
-  static restar(a, b) {
+
+function restar(a, b) {
     return a - b;
-  }
 }
-
-module.exports = Operaciones;
-
 // Funcion que ns ayuda a exportar al otro archivo esta seccion de codigo
 module.exports = restar;
+
