96e128cb8d769b677511f68147df3bfc0e5b5687
RuthPina8 <124050978@upq.edu.mx>
2025-10-09 16:59:55 -0600

    Practica 5
    Practica realizada en día 09/10/25 en la cual se llevo a cabo la creación de botones que aumenten, disminuyan o reinicien el contador

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,23 @@
+//1. imports: Zona de importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button} from 'react-native';
+import React, { useState } from 'react';
+//2. Main: Zona de componentes que van a estar a la vista
 export default function App() {
+const[Contador,setContador]=useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo!৻(  •̀ ᗜ •́  ৻)</Text>
+      <Text>৻(  •̀ ᗜ •́  ৻)</Text>
+      <Text>Contador:{Contador}</Text>
+      <Button title='Agregar' onPress={()=>setContador(Contador+1)}/>
+      <Button title='Quitar' onPress={()=>setContador(Contador-1)}/>
+      <Button title='Reiniciar' onPress={()=>setContador(0)}/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+//3. Estilos: Zona estetica y de posicionamiento  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
