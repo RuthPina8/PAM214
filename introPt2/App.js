7da687c738faeade21af1761e9a468004998fcf6
RuthPina8 <124050978@upq.edu.mx>
2025-10-09 12:24:54 -0600

    Practica4
    Finalizacion de la practica 4 en la que se trabajo la creacion de  archivos para react native

--- introPt2/App.js
+++ introPt2/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo!৻(  •̀ ᗜ •́  ৻)</Text>
+      <Text>Holi de nuevo!</Text>
       <StatusBar style="auto" />
     </View>
   );
